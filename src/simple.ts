7b5109d06e0296774d3350dc7d16553acbd8d235
Jason Gedge <jason@gadget.dev>
2022-03-28 20:09:26 -0400

    Fix undefined literal types

--- src/simple.ts
+++ src/simple.ts
@@ -1,6 +1,7 @@
 import { IAnyType, ISimpleType, SnapshotIn, types } from "mobx-state-tree";
-import { Primitives } from "mobx-state-tree/dist/internal";
 import { BaseType } from "./base";
+
+export type Primitives = string | number | boolean | Date | null | undefined;
 
 export class SimpleType<T, MSTType extends ISimpleType<any>> extends BaseType<T, T, MSTType> {
   static for<MSTType extends IAnyType>(mstType: MSTType): SimpleType<SnapshotIn<MSTType>, MSTType> {
@@ -9,12 +10,25 @@ export class SimpleType<T, MSTType extends ISimpleType<any>> extends BaseType<T,
 
   createReadOnly(snapshot?: this["InputType"]): this["InstanceType"] {
     if (snapshot === undefined) {
-      throw new Error("can't initial simple type with undefined");
+      throw new Error("can't initialize simple type with undefined");
     }
     return snapshot;
   }
 }
 
-export const literal = <T extends Primitives>(value: T): SimpleType<T, ISimpleType<T>> => {
-  return new SimpleType(`literal<${value}>`, types.literal(value));
+export class LiteralType<T extends Primitives> extends SimpleType<T, ISimpleType<T>> {
+  constructor(readonly value: T) {
+    const mstType = types.literal<T>(value);
+    super(mstType.name, mstType);
+  }
+
+  createReadOnly(snapshot?: this["InputType"]): this["InstanceType"] {
+    if (snapshot !== this.value) {
+      throw new Error(`expected literal type to be initialized with ${this.value}`);
+    }
+    return this.value;
+  }
+}
+export const literal = <T extends Primitives>(value: T): LiteralType<T> => {
+  return new LiteralType(value);
 };
