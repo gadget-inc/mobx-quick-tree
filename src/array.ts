66a13e9f0e64db4e50e4bb9e078597fa78930397
Jason Gedge <jason@gadget.dev>
2022-03-30 13:21:46 -0400

    Create a mirror of IMSTArray

--- src/array.ts
+++ src/array.ts
@@ -1,24 +1,49 @@
-import { types } from "mobx-state-tree";
-import { IArrayType } from "mobx-state-tree/dist/internal";
+import { IAnyType as IAnyMSTType, IArrayType, IMSTArray, types } from "mobx-state-tree";
 import { BaseType, InstantiateContext, setParent, setType } from "./base";
 import type { IAnyType } from "./types";
 
+export class QuickArray<T, M extends IAnyMSTType> extends Array<T> implements IMSTArray<M> {
+  static get [Symbol.species]() {
+    return Array;
+  }
+
+  spliceWithArray(_index: number, _deleteCount?: number, _newItems?: M["Type"][]): M["Type"][] {
+    throw new Error("cannot spliceWithArray on a QuickArray instance");
+  }
+
+  clear(): M["Type"][] {
+    throw new Error("cannot clear a QuickArray instance");
+  }
+
+  replace(_newItems: M["Type"][]): M["Type"][] {
+    throw new Error("cannot replace a QuickArray instance");
+  }
+
+  remove(_value: M["Type"]): boolean {
+    throw new Error("cannot remove from a QuickArray instance");
+  }
+
+  toJSON(): M["Type"][] {
+    return this;
+  }
+}
+
 export class ArrayType<T extends IAnyType> extends BaseType<
   ReadonlyArray<T["InputType"]>,
-  ReadonlyArray<T["InstanceType"]>,
+  QuickArray<T["InstanceType"], T["mstType"]>,
   IArrayType<T["mstType"]>
 > {
   constructor(readonly childrenType: T) {
     super(`array<${childrenType.name}>`, types.array(childrenType.mstType));
   }
 
-  instantiate(snapshot: this["InputType"], context: InstantiateContext): this["InstanceType"] {
-    const array: T["InstanceType"][] = [];
+  instantiate(snapshot: this["InputType"] | undefined, context: InstantiateContext): this["InstanceType"] {
+    const array = new QuickArray<T["InstanceType"], T["mstType"]>(snapshot?.length ?? 0);
     if (snapshot) {
-      snapshot.forEach((child) => {
+      snapshot.forEach((child, index) => {
         const item = this.childrenType.instantiate(child, context);
         setParent(item, array);
-        array.push(item);
+        array[index] = item;
       });
     }
 
