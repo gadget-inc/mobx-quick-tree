91278dece26837804a88cae14c92689524b24638
Jason Gedge <jason@gadget.dev>
2022-04-16 14:19:20 -0400

    Make the $identifier prop non-enumerable

--- src/model.ts
+++ src/model.ts
@@ -207,7 +207,7 @@ export class ModelType<Props extends ModelProperties, Others> extends BaseType<
 
     if (this.identifierProp) {
       const id = instance[this.identifierProp];
-      instance[$identifier] = id;
+      Object.defineProperty(instance, $identifier, { value: id });
       context.referenceCache.set(id, instance);
     }
 
