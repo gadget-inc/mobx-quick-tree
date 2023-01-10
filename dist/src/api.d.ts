a1ffcc8ce39b3fb758c7aef62b945811b54aa6d4
Harry Brundage <harry.brundage@gmail.com>
2023-01-09 23:11:52 -0500

    gitpkg

---
+++ dist/src/api.d.ts
@@ -0,0 +1,51 @@
+import type { IAnyComplexType as MSTAnyComplexType, IAnyType as MSTAnyType, IDisposer } from "mobx-state-tree";
+import type { FlowReturn } from "mobx-state-tree/dist/internal";
+import type { CreateTypes, IAnyComplexType, IAnyModelType, IAnyStateTreeNode, IAnyType, IArrayType, IMapType, Instance, IReferenceType, IStateTreeNode, IType, SnapshotIn } from "./types";
+export { addDisposer, addMiddleware, applyPatch, clone, createActionTrackingMiddleware2, destroy, detach, escapeJsonPath, getIdentifier, getPath, getPathParts, hasParent, isActionContextThisOrChildOf, isAlive, isValidReference, joinJsonPath, onAction, onPatch, recordPatches, resolvePath, setLivelinessChecking, splitJsonPath, tryReference, typecheck, walk, } from "mobx-state-tree";
+export { action, ClassModel, register, view } from "./class-model";
+export { getSnapshot } from "./snapshot";
+export declare const isType: (value: any) => value is IAnyType;
+/**
+ * Returns true if the given object is a complex type in observable mode
+ * @param value any object
+ * @returns
+ */
+export declare const isStateTreeNode: (value: any) => value is IStateTreeNode<IAnyType>;
+/**
+ * Returns true if the given object is a complex type in readonly mode
+ * @param value any object
+ * @returns
+ */
+export declare const isReadOnlyNode: (value: any) => value is IStateTreeNode<IAnyType>;
+export declare const getParent: <T extends IAnyType>(value: IAnyStateTreeNode, depth?: number) => Instance<T>;
+export declare function getParentOfType<T extends IAnyComplexType | MSTAnyComplexType>(value: IAnyStateTreeNode, type: T): Instance<T>;
+export declare function getType(value: IAnyStateTreeNode): MSTAnyComplexType | IAnyComplexType;
+export declare function getEnv<Env = any>(value: IAnyStateTreeNode): Env;
+export declare const getRoot: <T extends IAnyType>(value: IAnyStateTreeNode) => Instance<T>;
+export declare const isRoot: (value: IAnyStateTreeNode) => boolean;
+export declare function resolveIdentifier<T extends IAnyModelType>(type: T, target: IStateTreeNode<IAnyType>, identifier: string): Instance<T> | undefined;
+export declare const applySnapshot: <C>(target: IStateTreeNode<IType<C, any, any>>, snapshot: C) => void;
+export declare const onSnapshot: <S>(target: IStateTreeNode<IType<any, S, any>>, callback: (snapshot: S) => void) => IDisposer;
+export declare const isArrayType: (value: IAnyType | MSTAnyType) => value is IArrayType<IAnyType>;
+export declare const isMapType: (value: IAnyType | MSTAnyType) => value is IMapType<IAnyType>;
+export declare const isModelType: (value: IAnyType | MSTAnyType) => value is IAnyModelType;
+export declare const isReferenceType: (value: IAnyType | MSTAnyType) => value is IReferenceType<IAnyComplexType>;
+export declare const isIdentifierType: (value: IAnyType | MSTAnyType) => value is IReferenceType<IAnyComplexType>;
+export declare function cast<O extends string | number | boolean | null | undefined = never>(snapshotOrInstance: O): O;
+export declare function cast<O extends IAnyType>(snapshotOrInstance: CreateTypes<O>): O;
+export declare function cast(snapshotOrInstance: never): never;
+/**
+ * Defines a new asynchronous action that uses `yield` instead of `await` for waiting for the results of promises
+ *
+ * Accepts an incoming generator function and returns a new async function with the right mobx-state-tree wrapping.
+ */
+export declare function flow<R, T, Args extends any[]>(generator: (this: T, ...args: Args) => Generator<PromiseLike<any>, R, any>): (...args: Args) => Promise<FlowReturn<R>>;
+/**
+ * Create a new root instance of a type
+ *
+ * @param type The type to create. Can be any type
+ * @param snapshot The snapshot of the type to create
+ * @param readOnly If true, the instance will be readOnly and created faster
+ * @param env The optional environment to decorate the whole tree with
+ */
+export declare const create: <T extends IAnyType>(type: T, snapshot?: SnapshotIn<T> | undefined, readOnly?: boolean, env?: Record<string, any>) => Instance<T>;
---
+++ dist/src/class-model.d.ts
@@ -0,0 +1,62 @@
+import "reflect-metadata";
+import type { IAnyType, IClassModelType, InputsForModel, InputTypesForModelProps, ModelPropertiesDeclaration, TypesForModelPropsDeclaration } from "./types";
+export type VolatileMetadata = {
+    type: "volatile";
+    property: string;
+    initializer: VolatileInitializer<any>;
+};
+type VolatileInitializer<T> = (instance: T) => Record<string, any>;
+/**
+ * Create a new base class for a ClassModel to extend. This is a function that you call that returns a class (a class factory).
+ *
+ * @example
+ *
+ * class MyModel extends ClassModel({ name: types.string }) {
+ *   @view
+ *   get upperCasedName() {
+ *     return this.name.toUpperCase();
+ *   }
+ *
+ *   @action
+ *   setName(name: string) {
+ *     this.name = name;
+ *   }
+ * }
+ */
+export declare const ClassModel: <PropsDeclaration extends ModelPropertiesDeclaration>(propertiesDeclaration: PropsDeclaration) => IClassModelType<TypesForModelPropsDeclaration<PropsDeclaration>, InputsForModel<InputTypesForModelProps<TypesForModelPropsDeclaration<PropsDeclaration>>>, import("./types").OutputTypesForModelProps<TypesForModelPropsDeclaration<PropsDeclaration>>>;
+/**
+ * Class decorator for registering MQT class models as setup.
+ *
+ * @example
+ * ```
+ *   @register
+ *   class Example extends ClassModel({ name: types.string }) {
+ *     @view
+ *     get bigName() {
+ *       return this.name.toUpperCase();
+ *     }
+ *   }
+ * ```
+ */
+export declare function register<Klass extends {
+    new (...args: any[]): {};
+}>(object: Klass): any;
+/**
+ * Function decorator for registering MST actions within MQT class models.
+ */
+export declare const action: (target: any, property: string, descriptor?: PropertyDescriptor) => void;
+/**
+ * Function decorator for registering MST views within MQT class models.
+ */
+export declare const view: (target: any, property: string, _descriptor: PropertyDescriptor) => void;
+/**
+ * Function decorator for registering MST volatiles within MQT class models.
+ */
+export declare function volatile(initializer: (instance: any) => any): (target: any, property: string) => void;
+/**
+ * Ensure a given type is registered if it requires registration.
+ * Throws an error if a type requires registration but has not been registered.
+ * @hidden
+ */
+export declare const ensureRegistered: (type: IAnyType) => void;
+export {};
