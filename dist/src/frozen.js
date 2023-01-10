f1f4af466da32cf39e052f252a6ee91ea02d2ac4
Harry Brundage <harry.brundage@gmail.com>
2023-01-09 23:14:42 -0500

    gitpkg

---
+++ dist/src/frozen.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.frozen = void 0;
+const mobx_state_tree_1 = require("mobx-state-tree");
+const base_1 = require("./base");
+class FrozenType extends base_1.BaseType {
+    constructor() {
+        super(mobx_state_tree_1.types.frozen());
+    }
+    instantiate(snapshot, _context) {
+        if (typeof snapshot == "function") {
+            throw new Error("frozen types can't be instantiated with functions");
+        }
+        return snapshot;
+    }
+    is(value) {
+        // Valid values for frozen types have to be serializable
+        return typeof value !== "function";
+    }
+}
+const frozen = () => {
+    return new FrozenType();
+};
+exports.frozen = frozen;
---
+++ dist/src/optional.js
@@ -0,0 +1,46 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.optional = void 0;
+const mobx_state_tree_1 = require("mobx-state-tree");
+const base_1 = require("./base");
+const class_model_1 = require("./class-model");
+class OptionalType extends base_1.BaseType {
+    constructor(type, defaultValueOrFunc, undefinedValues) {
+        super(undefinedValues
+            ? mobx_state_tree_1.types.optional(type.mstType, defaultValueOrFunc, undefinedValues)
+            : mobx_state_tree_1.types.optional(type.mstType, defaultValueOrFunc));
+        this.type = type;
+        this.defaultValueOrFunc = defaultValueOrFunc;
+        this.undefinedValues = undefinedValues;
+    }
+    instantiate(snapshot, context) {
+        if (this.undefinedValues) {
+            if (this.undefinedValues.includes(snapshot)) {
+                snapshot = this.defaultValue;
+            }
+        }
+        else if (snapshot === undefined) {
+            snapshot = this.defaultValue;
+        }
+        return this.type.instantiate(snapshot, context);
+    }
+    is(value) {
+        if (this.undefinedValues) {
+            if (this.undefinedValues.includes(value)) {
+                return true;
+            }
+        }
+        else if (value === undefined) {
+            return true;
+        }
+        return this.type.is(value);
+    }
+    get defaultValue() {
+        return this.defaultValueOrFunc instanceof Function ? this.defaultValueOrFunc() : this.defaultValueOrFunc;
+    }
+}
+const optional = (type, defaultValue, undefinedValues) => {
+    (0, class_model_1.ensureRegistered)(type);
+    return new OptionalType(type, defaultValue, undefinedValues);
+};
+exports.optional = optional;
