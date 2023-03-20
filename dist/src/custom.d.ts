41d7d04b40f758d1d9e0c78e08cd0f4b25949241
Harry Brundage <harry.brundage@gmail.com>
2023-03-20 16:26:56 -0400

    gitpkg

---
+++ dist/src/custom.d.ts
@@ -0,0 +1,3 @@
+import type { CustomTypeOptions } from "mobx-state-tree";
+import type { IType } from "./types";
+export declare const custom: <InputType, OutputType>(options: CustomTypeOptions<InputType, OutputType>) => IType<InputType, OutputType, OutputType>;
---
+++ dist/src/frozen.d.ts
@@ -0,0 +1,2 @@
+import type { ISimpleType } from "./types";
+export declare const frozen: <T = any>() => ISimpleType<T>;
