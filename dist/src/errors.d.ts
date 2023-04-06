dc2e476e61ef32e31179ab3e782b6c0232f8bd88
Harry Brundage <harry.brundage@gmail.com>
2023-04-06 10:02:08 -0400

    gitpkg

---
+++ dist/src/errors.d.ts
@@ -0,0 +1,6 @@
+/** Thrown when an action is invoked on a read-only model instance */
+export declare class CantRunActionError extends Error {
+}
+/** Thrown when an invalid registration is passed to the class model register function */
+export declare class RegistrationError extends Error {
+}
