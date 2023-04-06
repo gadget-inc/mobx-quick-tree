11b0f5de340fe8eaa22a6a98e608b40ae9d32434
Harry Brundage <harry.brundage@gmail.com>
2023-04-06 10:51:25 -0400

    gitpkg

---
+++ dist/src/array.d.ts
@@ -0,0 +1,14 @@
+import { $readOnly, $type } from "./symbols";
+import type { IAnyType, IArrayType, IMSTArray, Instance } from "./types";
+export declare class QuickArray<T extends IAnyType> extends Array<Instance<T>> implements IMSTArray<T> {
+    static get [Symbol.species](): ArrayConstructor;
+    [$type]?: [this] | [any];
+    get [Symbol.toStringTag](): "Array";
+    get [$readOnly](): boolean;
+    spliceWithArray(_index: number, _deleteCount?: number, _newItems?: Instance<T>[]): Instance<T>[];
+    clear(): Instance<T>[];
+    replace(_newItems: Instance<T>[]): Instance<T>[];
+    remove(_value: Instance<T>): boolean;
+    toJSON(): Instance<T>[];
+}
+export declare const array: <T extends IAnyType>(childrenType: T) => IArrayType<T>;
