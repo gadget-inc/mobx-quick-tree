b00169b4d2133959bef5569a6168d09acae65b33
Harry Brundage <harry.brundage@gmail.com>
2023-04-21 13:50:30 -0400

    gitpkg

---
+++ dist/src/maybe.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.maybeNull = exports.maybe = void 0;
+const mobx_state_tree_1 = require("mobx-state-tree");
+const base_1 = require("./base");
+const class_model_1 = require("./class-model");
+class MaybeType extends base_1.BaseType {
+    constructor(type) {
+        super(mobx_state_tree_1.types.maybe(type.mstType));
+        this.type = type;
+    }
+    instantiate(snapshot, context, parent) {
+        if (snapshot === undefined) {
+            return undefined;
+        }
+        return this.type.instantiate(snapshot, context, parent);
+    }
+    is(value) {
+        if (value === undefined) {
+            return true;
+        }
+        return this.type.is(value);
+    }
+}
+class MaybeNullType extends base_1.BaseType {
+    constructor(type) {
+        super(mobx_state_tree_1.types.maybeNull(type.mstType));
+        this.type = type;
+    }
+    instantiate(snapshot, context, parent) {
+        if (snapshot === undefined || snapshot === null) {
+            // Special case for things like types.frozen, or types.literal(undefined), where MST prefers the subtype over maybeNull
+            if (this.type.is(snapshot)) {
+                return this.type.instantiate(snapshot, context, parent);
+            }
+            return null;
+        }
+        return this.type.instantiate(snapshot, context, parent);
+    }
+    is(value) {
+        if (value === undefined || value === null) {
+            return true;
+        }
+        return this.type.is(value);
+    }
+}
+const maybe = (type) => {
+    (0, class_model_1.ensureRegistered)(type);
+    return new MaybeType(type);
+};
+exports.maybe = maybe;
+const maybeNull = (type) => {
+    (0, class_model_1.ensureRegistered)(type);
+    return new MaybeNullType(type);
+};
+exports.maybeNull = maybeNull;
---
+++ dist/src/snapshot.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSnapshot = void 0;
+const mobx_state_tree_1 = require("mobx-state-tree");
+const api_1 = require("./api");
+const array_1 = require("./array");
+const map_1 = require("./map");
+const symbols_1 = require("./symbols");
+function getSnapshot(value) {
+    if ((0, mobx_state_tree_1.isStateTreeNode)(value)) {
+        return (0, mobx_state_tree_1.getSnapshot)(value);
+    }
+    return snapshot(value);
+}
+exports.getSnapshot = getSnapshot;
+// modeltype => snapshot cache
+const cache = new WeakMap();
+const snapshot = (value) => {
+    if (value instanceof array_1.QuickArray) {
+        return value.map((v) => snapshot(v));
+    }
+    if (value instanceof map_1.QuickMap) {
+        return Object.fromEntries(Array.from(value.entries()).map(([k, v]) => [k, snapshot(v)]));
+    }
+    if (value instanceof Date) {
+        return value.getTime();
+    }
+    if ((0, api_1.isStateTreeNode)(value)) {
+        const type = (0, api_1.getType)(value);
+        if ((0, api_1.isModelType)(type)) {
+            const isReadOnly = (0, api_1.isReadOnlyNode)(value);
+            if (isReadOnly) {
+                // return a previously computed snapshot for a readonly node if it is available
+                const cached = cache.get(value);
+                if (cached)
+                    return cached;
+            }
+            const modelSnapshot = {};
+            for (const name in type.properties) {
+                const propType = type.properties[name];
+                if ((0, api_1.isReferenceType)(propType)) {
+                    const maybeRef = value[name];
+                    modelSnapshot[name] = maybeRef?.[symbols_1.$identifier];
+                }
+                else {
+                    modelSnapshot[name] = snapshot(value[name]);
+                }
+            }
+            if (isReadOnly) {
+                cache.set(value, modelSnapshot);
+            }
+            return modelSnapshot;
+        }
+    }
+    return value;
+};
