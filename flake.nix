b43d39eba7d3eb0ac87743eff772b23fac347423
Harry Brundage <harry.brundage@gmail.com>
2023-11-07 17:11:05 -0500

    Add nix setup for development and upgrade node

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "mobx-quick-tree development environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    (flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ]
+      (system: nixpkgs.lib.fix (flake:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+
+          packages =
+            rec {
+              bash = pkgs.bash;
+              nodejs = pkgs.nodejs_21;
+              pnpm = pkgs.nodejs_21.pkgs.pnpm;
+            };
+
+          devShell = pkgs.mkShell {
+            packages = builtins.attrValues packages;
+          };
+        }
+      )));
+}
