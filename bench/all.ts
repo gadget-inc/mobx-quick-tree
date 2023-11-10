d60267e963e820ab7fc3d6f4ff0e464b6a203957
Harry Brundage <harry.brundage@gmail.com>
2023-11-10 08:59:00 -0500

    Add codspeed continuous benchmarking for CI

--- bench/all.ts
+++ bench/all.ts
@@ -1,17 +1,18 @@
 import { Suite } from "benchmark";
+import { withCodSpeed } from "@codspeed/benchmark.js-plugin";
 import findRoot from "find-root";
 import fs from "fs";
 import { LargeRoot } from "../spec/fixtures/LargeRoot";
 import { FruitAisle } from "../spec/fixtures/FruitAisle";
 import { TestClassModel } from "../spec/fixtures/TestClassModel";
-import { TestModelSnapshot } from "../spec/fixtures/TestModel";
+import { BigTestModelSnapshot, TestModelSnapshot } from "../spec/fixtures/TestModel";
 
 const root = findRoot(__dirname);
 const largeRoot = JSON.parse(fs.readFileSync(root + "/spec/fixtures/large-root-snapshot.json", "utf8"));
 const fruitAisle = JSON.parse(fs.readFileSync(root + "/spec/fixtures/fruit-aisle-snapshot.json", "utf8"));
-const suite = new Suite("instantiating model classes");
+const suite = withCodSpeed(new Suite("instantiating model classes"));
 
-suite
+void suite
   .add("instantiating a small root", function () {
     TestClassModel.createReadOnly(TestModelSnapshot);
   })
@@ -20,6 +21,9 @@ suite
   })
   .add("instantiating a large union", function () {
     FruitAisle.createReadOnly(fruitAisle);
+  })
+  .add("instantiating a diverse root", function () {
+    TestClassModel.createReadOnly(BigTestModelSnapshot);
   })
   .on("start", function () {
     console.profile();
