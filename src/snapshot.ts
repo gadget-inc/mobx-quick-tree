4bb51585fbd51eeccd6cf6b8998a913ad36c2d2e
Jason Gedge <jason@gadget.dev>
2024-05-08 18:01:03 -0400

    Ensure we properly snapshot reference types in maps/arrays

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -1,8 +1,8 @@
 import type { IStateTreeNode as MSTStateTreeNode } from "mobx-state-tree";
 import { getSnapshot as mstGetSnapshot, isStateTreeNode as mstIsStateTreeNode } from "mobx-state-tree";
 import { getType, isModelType, isReferenceType, isStateTreeNode } from "./api";
-import { QuickArray } from "./array";
-import { QuickMap } from "./map";
+import { ArrayType, QuickArray } from "./array";
+import { MapType, QuickMap } from "./map";
 import { $identifier } from "./symbols";
 import type { IAnyType, IStateTreeNode, SnapshotOut } from "./types";
 
@@ -16,11 +16,21 @@ export function getSnapshot<T extends IAnyType>(value: IStateTreeNode<T>): Snaps
 
 const snapshot = (value: any): unknown => {
   if (value instanceof QuickArray) {
-    return Array.from(value.map((v) => snapshot(v)));
+    const type = getType(value) as ArrayType<IAnyType>;
+    const childrenAreReferences = isReferenceType(type.childrenType);
+
+    return Array.from(value.map((v) => (childrenAreReferences ? v[$identifier] : snapshot(v))));
   }
 
   if (value instanceof QuickMap) {
-    return Object.fromEntries(Array.from(value.entries()).map(([k, v]) => [k, snapshot(v)]));
+    const type = getType(value) as MapType<IAnyType>;
+    const childrenAreReferences = isReferenceType(type.childrenType);
+
+    return Object.fromEntries(
+      Array.from(value.entries()).map(([k, v]) => {
+        return [k, childrenAreReferences ? v[$identifier] : snapshot(v)];
+      })
+    );
   }
 
   if (value instanceof Date) {
