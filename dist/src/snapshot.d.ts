5a0a762ec10900965af1915c0a708144ec152c1e
Harry Brundage <harry.brundage@gmail.com>
2024-06-03 16:45:18 -0400

    gitpkg

---
+++ dist/src/snapshot.d.ts
@@ -0,0 +1,5 @@
+import type { IAnyType, IStateTreeNode, SnapshotOut } from "./types";
+import type { SnapshottedViewMetadata } from "./class-model";
+export declare function getSnapshot<T extends IAnyType>(value: IStateTreeNode<T>): SnapshotOut<T>;
+/** @internal */
+export declare const storeViewOnSnapshot: (node: Record<string, any>, view: SnapshottedViewMetadata, snapshot: any) => void;
